e922094403d936f556b6aec635f96a6570d3a2d4
zhangxv <xuzhang@thoughtworks.com>
2015-03-03 19:52:52 +0800

    print Diamond with Name

--- src/com/raingxm/Main.java
+++ src/com/raingxm/Main.java
@@ -2,9 +2,9 @@ package com.raingxm;
 
 public class Main {
 
-    public static void printIsoscelesTriangle(int n) {
+    public static void printIsoscelesTriangle(int n,int space) {
         for(int i=1; i<=n; i++) {
-            printSpace(n-i);
+            printSpace(n-i + space);
             printAsterisk(2*i - 1);
             printSpace(n-i);
             System.out.println();
@@ -12,7 +12,13 @@ public class Main {
     }
 
     public static void printDiamond(int n) {
-        printIsoscelesTriangle(n);
+        printIsoscelesTriangle(n, 0);
+        printReverseTriangle(n-1, 1);
+    }
+
+    public static void printDiamondWithName(int n, String name) {
+        printIsoscelesTriangle(n - 1, 1);
+        System.out.println(name);
         printReverseTriangle(n-1, 1);
     }
 
@@ -39,6 +45,7 @@ public class Main {
 
     public static void main(String[] args) {
         //printIsoscelesTriangle(3);
-        printDiamond(3);
+//        printDiamond(3);
+        printDiamondWithName(3, "zhangxv");
     }
 }
